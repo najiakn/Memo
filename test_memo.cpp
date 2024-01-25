74e3ef0302cda583a53ec2c07a2a009eebd89eb7
najia <najiaknizi199@gmail.com>
2024-01-25 16:40:07 +0100

    function modifier + retire le code

--- test_memo.cpp
+++ test_memo.cpp
@@ -1,104 +1,5 @@
 #include <stdio.h>
 #include <stdlib.h>
-
-#define PRO_Tache 100
-
-typedef struct {
-    int jour;
-    int mois;
-    int annee;
-} date_eche;
-
-typedef struct {
-    int code;
-    char nom[20];
-    char description[200];
-    date_eche date;
-    char priorite[100];
-} taches;
-
-taches ajouter() {
-
-    taches tache1;
-
-    printf("Remplissez la tache :\n");
-    printf("Tapez le code : ");
-    scanf("%d", &(tache1.code));
-
-    printf("Tapez le nom : ");
-    scanf("%s", tache1.nom);
-
-    printf("Tapez la description : ");
-    scanf("%s", tache1.description);
-
-    printf("Tapez la date d'echeance :\n");
-    printf("Tapez le jour : ");
-    scanf("%d", &(tache1.date.jour));
-
-    printf("Tapez le mois : ");
-    scanf("%d", &(tache1.date.mois));
-
-    printf("Tapez l'annee : ");
-    scanf("%d", &(tache1.date.annee));
-
-    printf("Tapez la priorite : ");
-    scanf("%s", tache1.priorite);
-
-    printf("Ajout avec succès !\n");
- 
-    return tache1;
-}
-
-void afficher(taches tache) {
-    printf("Code : %d, Nom : %s, Description : %s, Date : %d/%d/%d, Priorite : %s\n",
-           tache.code, tache.nom, tache.description,
-           tache.date.jour, tache.date.mois, tache.date.annee,
-           tache.priorite);
-}
-
-void menu() {
-    int choix = 1;
-    taches tache1;
-
-    while (choix >= 1 && choix <= 4) {
-        printf("1--------------Afficher les taches----------------\n");
-        printf("2--------------Ajouter une tache-------------------\n");
-        printf("3--------------Supprimer une tache-----------------\n");
-        printf("4--------------Fermer-------------------------------\n");
-
-        scanf("%d", &choix);
-
-        switch (choix) {
-            case 1:
-                printf("Voici les taches :\n");
-                afficher(tache1);
-                break;
-
-            case 2:
-                printf("Ajouter une tache :\n");
-                tache1 = ajouter();
-                break;
-
-            case 3:
-                printf("Supprimer une tache :\n");
-              
-                break;
-
-            case 4:
-                printf("Fermeture demandée. Au revoir !\n");
-                return;
-
-            default:
-                printf("Option non valide.\n");
-        }
-    }
-}
-
-int main() {
-    menu();
-
-    return 0;#include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
 #define PRO_Tache 100
 
@@ -157,7 +58,7 @@ void afficher(taches tache[], int tache_nombre) {
 }
 void modifier(taches tache[], int tache_nombre) {
     char nom[PRO_Tache];
-    printf("Tapez le nom de la tache a modifier : ");
+    printf("Tapez le nom de la tache à modifier : ");
     scanf("%s", nom);
 
     int index = -1;
@@ -185,9 +86,9 @@ void modifier(taches tache[], int tache_nombre) {
         printf("Tapez la nouvelle priorite : ");
         scanf("%s", tache[index].priorite);
 
-        printf("Modification avec succes !\n");
+        printf("Modification avec succès !\n");
     } else {
-        printf("Tache non trouvee avec le nom %s\n", nom);
+        printf("Tache non trouvée avec le nom %s\n", nom);
     }
 }
 
